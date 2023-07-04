3a933dab7523997bbb5c57f35357a3c97243df19
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2023-07-04 13:01:56 +0700

    intent register

--- app/src/main/java/com/habibfr/budget_buddy/RegisterActivity.java
+++ app/src/main/java/com/habibfr/budget_buddy/RegisterActivity.java
@@ -2,13 +2,29 @@ package com.habibfr.budget_buddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    TextView txtLoginInRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        txtLoginInRegister = findViewById(R.id.txtLoginInRegister);
+
+        txtLoginInRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
