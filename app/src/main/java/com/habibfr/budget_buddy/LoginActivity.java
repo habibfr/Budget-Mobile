a475758810c595f9c7d6013a58102f358a0c7f05
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2023-07-04 13:01:44 +0700

    intent login

--- app/src/main/java/com/habibfr/budget_buddy/LoginActivity.java
+++ app/src/main/java/com/habibfr/budget_buddy/LoginActivity.java
@@ -2,13 +2,33 @@ package com.habibfr.budget_buddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView txtRegisterInLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtRegisterInLogin = findViewById(R.id.txtSignUpInLogin);
+
+
+        txtRegisterInLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
     }
 }
