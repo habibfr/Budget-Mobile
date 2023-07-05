b86cdd8919f14feca9c797d8e91bc9919063c9d5
Excel951 <abigailexcelsisdeo@gmail.com>
2023-07-05 13:48:44 +0700

    changes on home, laporan activity, laporan xml

--- app/src/main/java/com/habibfr/budget_buddy/LaporanActivity.java
+++ app/src/main/java/com/habibfr/budget_buddy/LaporanActivity.java
@@ -1,14 +1,38 @@
 package com.habibfr.budget_buddy;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class LaporanActivity extends AppCompatActivity {
+
+    ImageView btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_laporan);
+
+        //        HIDE ACTION BAR
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.hide();
+
+        btnBack = findViewById(R.id.imageArrowdown);
+
+//        function collection
+        btnBackOnClick();
+    }
+
+    private void btnBackOnClick() {
+        btnBack.setOnClickListener(view -> {
+            Intent intent = new Intent(LaporanActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
