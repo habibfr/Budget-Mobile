8beacc072f7106bf07e0b9fff88a58d4febcb126
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2023-07-05 13:33:34 +0700

    add comment link

--- app/src/main/java/com/habibfr/budget_buddy/LoginActivity.java
+++ app/src/main/java/com/habibfr/budget_buddy/LoginActivity.java
@@ -72,8 +72,8 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new Konektor(LoginActivity.this, "http://192.168.1.11/mobile/users/cek_login.php", new Uri.Builder().appendQueryParameter("email", editEmail.getText().toString()).appendQueryParameter("password", editPassword.getText().toString())).execute();
-
+                // ganti sesuai php file masing-masing
+                new Konektor(LoginActivity.this, "http://192.168.43.37/pbm/uas/users/cek_login.php", new Uri.Builder().appendQueryParameter("email", editEmail.getText().toString()).appendQueryParameter("password", editPassword.getText().toString())).execute();
             }
         });
 
