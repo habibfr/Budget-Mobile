4abff2451c02710a883af1a086c2dd6802203330
Abiansyah19 <21410100029@dinamika.ac.id>
2023-07-08 19:58:29 +0700

    ganti detail dan itu

--- app/src/main/java/com/habibfr/budget_buddy/DetailActivity.java
+++ app/src/main/java/com/habibfr/budget_buddy/DetailActivity.java
@@ -2,17 +2,26 @@ package com.habibfr.budget_buddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -21,6 +30,7 @@ public class DetailActivity extends AppCompatActivity {
     private TextView tanggal;
     private TextView total;
     private TextView ket;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +42,24 @@ public class DetailActivity extends AppCompatActivity {
         total = findViewById(R.id.amount);
         ket = findViewById(R.id.keterangan);
         executeRequest();
+        btn=findViewById(R.id.btn_ok);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DetailActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     private void executeRequest() {
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = "http://10.0.2.2/uas/transactions/get_transaction.php";
+        int user_id=getIntent().getIntExtra("user_id", 0);
+        int transaction_id=getIntent().getIntExtra("transaction_id", 0);
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
@@ -50,6 +71,8 @@ public class DetailActivity extends AppCompatActivity {
                                 TransactionData transactionData = responseData.getData();
                                 Log.d("test", "onResponse: "+ transactionData.getTransactionId());
                                 if (transactionData != null) {
+                                    Log.d("error", "getParams: "+user_id);
+                                    Log.d("error", "getParams: "+transaction_id);
                                     tipe_transaksi.setText(String.valueOf(transactionData.getTransactionId()));
                                     judul.setText(String.valueOf(transactionData.getUserId()));
                                     tanggal.setText(transactionData.getTitle());
@@ -70,7 +93,15 @@ public class DetailActivity extends AppCompatActivity {
             public void onErrorResponse(VolleyError error) {
                 Log.e("Error", error.getLocalizedMessage());
             }
-        });
+        }) {
+            protected Map<String, String> getParams() {
+                Map<String, String> paramV = new HashMap<>();
+                paramV.put("user_id", String.valueOf(user_id));
+                paramV.put("transaction_id", String.valueOf(transaction_id));
+                return paramV;
+            }
+        };
+
         queue.add(stringRequest);
     }
 
